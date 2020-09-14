3058ed4b25c40ccb661879105af94c670b734027
Ali <ali.nili128@gmail.com>
2020-09-14 13:56:41 -0700

    print the exceptions

--- src/main/java/cli/CliApp.java
+++ src/main/java/cli/CliApp.java
@@ -21,11 +21,10 @@ public class CliApp implements Callable<String> {
     public String call() throws Exception {
         String outputFileName = inputPath.getFileName().toString().split("\\.")[0];
         Parser parser = new Parser(inputPath, packageName, outputFileName);
-
         try {
             parser.generate();
         } catch (ParseException e) {
-            return e.getMessage() + " at line " + e.getErrorOffset();
+            System.out.println(e.getMessage() + " at line " + e.getErrorOffset());
         }
 
         return outputFileName + " was generated in package " + packageName;
